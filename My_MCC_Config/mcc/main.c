110bd820526d0e52886392f34d1780ac0a24dffa
alcp1 <alexpozzer@gmail.com>
2026-05-15 21:17:39 -0300

    Initial Version

--- My_MCC_Config/mcc/main.c
+++ My_MCC_Config/mcc/main.c
@@ -25,6 +25,8 @@
 // INTERNAL GLOBAL VARIABLES
 //----------------------------------------------------------------------------//
 static volatile unsigned char g_mainCounter;
+static volatile unsigned char g_tempI2CData;
+static volatile i2c_client_error_t g_errorState;
 
 //----------------------------------------------------------------------------//
 // INTERNAL FUNCTIONS
@@ -34,6 +36,65 @@ static volatile unsigned char g_mainCounter;
 void Timer_Callback_20ms(void)
 {
     g_mainCounter++;
+}
+
+static bool Client_Application(i2c_client_transfer_event_t event)
+{
+    switch (event)
+    {
+        //----------------------------
+        // Address Match Event
+        //----------------------------
+        case I2C_CLIENT_TRANSFER_EVENT_ADDR_MATCH:
+            if (I2C0_Client.TransferDirGet() == I2C_CLIENT_TRANSFER_DIR_WRITE)
+            {
+                // Write from Host
+            }
+            else
+            {
+                // Read from Host
+            }
+            break;
+
+        //----------------------------
+        // Data from Host to be received
+        //----------------------------
+        case I2C_CLIENT_TRANSFER_EVENT_RX_READY:
+            g_tempI2CData  = I2C0_Client.ReadByte();
+            break;
+
+        //----------------------------
+        // Data to be sent to Host
+        //----------------------------
+        case I2C_CLIENT_TRANSFER_EVENT_TX_READY:
+            I2C0_Client.WriteByte(g_tempI2CData);
+            break;
+
+        //----------------------------
+        // End of communication
+        //----------------------------
+        case I2C_CLIENT_TRANSFER_EVENT_STOP_BIT_RECEIVED:
+            break;
+
+        //----------------------------
+        // Error Handler
+        //----------------------------
+        case I2C_CLIENT_TRANSFER_EVENT_ERROR:
+            g_errorState = I2C0_Client.ErrorGet();
+            if(g_errorState == I2C_CLIENT_ERROR_BUS_ERROR)
+            {
+                // Bus Error Handling
+            }
+            else if(g_errorState == I2C_CLIENT_ERROR_COLLISION)
+            {
+                // Collision Error Handling
+            }
+            break;
+
+        default:
+        break;
+    }
+    return true;
 }
 
 // main function
@@ -47,6 +108,7 @@ int main(void)
     IO_SET_SHDN_INACTIVE();
     led_init();
     adc_init();
+    I2C0_Client.CallbackRegister(Client_Application);
     // Endless loop
     g_mainCounter = 0;
     while(1)
